d12658dac3ebbb292a257e4b1abd18a0429bb09f
tulios <ornelas.tulio@gmail.com>
2013-08-03 22:45:03 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+  "flag"
+  "fmt"
+  "net/http"
+  "errors"
+  "io/ioutil"
+  "regexp"
+  "sync"
+)
+
+const (
+  QUEUE_SIZE = 10
+)
+
+type Options struct {
+  url string
+  target string
+  workers int
+}
+
+type Link struct {
+  filename string
+  url string
+}
+
+
+func main() {
+  var wg sync.WaitGroup
+  opts := extractParams()
+
+  fmt.Printf("Downloading %s\n", opts.url)
+  bytes, err := fetchUrl(opts.url)
+  if err != nil { panic(err) }
+
+  links := extractLinks(bytes, opts)
+  fmt.Printf("%d files\n", len(links))
+
+  queue := make(chan Link, QUEUE_SIZE)
+  for i := 0; i < opts.workers; i++ {
+    wg.Add(1)
+    go worker(i + 1, queue, opts, &wg)
+  }
+
+  for _, link := range links {
+    queue <- link
+  }
+
+  fmt.Println("Closing...")
+  close(queue)
+  wg.Wait()
+}
+
+func worker(index int, queue <-chan Link, opts Options, wg *sync.WaitGroup) {
+  defer wg.Done()
+  for link := range queue {
+    fmt.Printf("Worker %d, downloading %s\n", index, link.url)
+    bytes, err := fetchUrl(link.url)
+
+    if err != nil {
+      fmt.Println(err)
+      continue
+    }
+
+    ioutil.WriteFile(opts.target + link.filename, bytes, 0644)
+  }
+}
+
+func fetchUrl(url string) ([]byte, error) {
+  resp, err := http.Get(url)
+  if err != nil { return nil, errors.New("Failed to fetch " + url) }
+  defer resp.Body.Close();
+
+  body, err := ioutil.ReadAll(resp.Body)
+  if (err != nil) { return nil, errors.New("Failed to parse " + url) }
+  return body, nil
+}
+
+func extractLinks(html []byte, opts Options) []Link {
+  fmt.Printf("Detecting subpaths...\n")
+  r := regexp.MustCompile("(?i)<td align=top><a href=\"([^\"]+)\">")
+  paths := r.FindAllSubmatch(html, -1)
+
+  var links []Link
+  for _, i := range paths {
+    path := string(i[1])
+    links = append(links, Link{filename: path, url: opts.url + path})
+  }
+
+  return links
+}
+
+func extractParams() Options {
+  url := flag.String("u", "", "URL")
+  target := flag.String("d", "/tmp", "Target directory")
+  workers := flag.Int("w", 2, "Number of workers")
+  flag.Parse()
+
+  return Options{ url: *url, target: *target, workers: *workers }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Download
+
+File downloader written in Go
+https://gist.github.com/fsouza/b0927a27f00d0748ca4b
+
+## Run
+
+```sh
+GOMAXPROCS=2 go run main.go -u http://www.textfiles.com/programming/ -d ~/Desktop/programming/ -w 4
+```
