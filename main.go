7ad8e690ec6b64b0ddceddceeae693202fa9bfff
tulios <ornelas.tulio@gmail.com>
2013-08-03 22:48:28 -0300

    gofmt

--- main.go
+++ main.go
@@ -1,101 +1,106 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "net/http"
-  "errors"
-  "io/ioutil"
-  "regexp"
-  "sync"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"sync"
 )
 
 const (
-  QUEUE_SIZE = 10
+	QUEUE_SIZE = 10
 )
 
 type Options struct {
-  url string
-  target string
-  workers int
+	url     string
+	target  string
+	workers int
 }
 
 type Link struct {
-  filename string
-  url string
+	filename string
+	url      string
 }
 
-
 func main() {
-  var wg sync.WaitGroup
-  opts := extractParams()
+	var wg sync.WaitGroup
+	opts := extractParams()
 
-  fmt.Printf("Downloading %s\n", opts.url)
-  bytes, err := fetchUrl(opts.url)
-  if err != nil { panic(err) }
+	fmt.Printf("Downloading %s\n", opts.url)
+	bytes, err := fetchUrl(opts.url)
+	if err != nil {
+		panic(err)
+	}
 
-  links := extractLinks(bytes, opts)
-  fmt.Printf("%d files\n", len(links))
+	links := extractLinks(bytes, opts)
+	fmt.Printf("%d files\n", len(links))
 
-  queue := make(chan Link, QUEUE_SIZE)
-  for i := 0; i < opts.workers; i++ {
-    wg.Add(1)
-    go worker(i + 1, queue, opts, &wg)
-  }
+	queue := make(chan Link, QUEUE_SIZE)
+	for i := 0; i < opts.workers; i++ {
+		wg.Add(1)
+		go worker(i+1, queue, opts, &wg)
+	}
 
-  for _, link := range links {
-    queue <- link
-  }
+	for _, link := range links {
+		queue <- link
+	}
 
-  fmt.Println("Closing...")
-  close(queue)
-  wg.Wait()
+	fmt.Println("Closing...")
+	close(queue)
+	wg.Wait()
 }
 
 func worker(index int, queue <-chan Link, opts Options, wg *sync.WaitGroup) {
-  defer wg.Done()
-  for link := range queue {
-    fmt.Printf("Worker %d, downloading %s\n", index, link.url)
-    bytes, err := fetchUrl(link.url)
+	defer wg.Done()
+	for link := range queue {
+		fmt.Printf("Worker %d, downloading %s\n", index, link.url)
+		bytes, err := fetchUrl(link.url)
 
-    if err != nil {
-      fmt.Println(err)
-      continue
-    }
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
 
-    ioutil.WriteFile(opts.target + link.filename, bytes, 0644)
-  }
+		ioutil.WriteFile(opts.target+link.filename, bytes, 0644)
+	}
 }
 
 func fetchUrl(url string) ([]byte, error) {
-  resp, err := http.Get(url)
-  if err != nil { return nil, errors.New("Failed to fetch " + url) }
-  defer resp.Body.Close();
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, errors.New("Failed to fetch " + url)
+	}
+	defer resp.Body.Close()
 
-  body, err := ioutil.ReadAll(resp.Body)
-  if (err != nil) { return nil, errors.New("Failed to parse " + url) }
-  return body, nil
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.New("Failed to parse " + url)
+	}
+	return body, nil
 }
 
 func extractLinks(html []byte, opts Options) []Link {
-  fmt.Printf("Detecting subpaths...\n")
-  r := regexp.MustCompile("(?i)<td align=top><a href=\"([^\"]+)\">")
-  paths := r.FindAllSubmatch(html, -1)
+	fmt.Printf("Detecting subpaths...\n")
+	r := regexp.MustCompile("(?i)<td align=top><a href=\"([^\"]+)\">")
+	paths := r.FindAllSubmatch(html, -1)
 
-  var links []Link
-  for _, i := range paths {
-    path := string(i[1])
-    links = append(links, Link{filename: path, url: opts.url + path})
-  }
+	var links []Link
+	for _, i := range paths {
+		path := string(i[1])
+		links = append(links, Link{filename: path, url: opts.url + path})
+	}
 
-  return links
+	return links
 }
 
 func extractParams() Options {
-  url := flag.String("u", "", "URL")
-  target := flag.String("d", "/tmp", "Target directory")
-  workers := flag.Int("w", 2, "Number of workers")
-  flag.Parse()
+	url := flag.String("u", "", "URL")
+	target := flag.String("d", "/tmp", "Target directory")
+	workers := flag.Int("w", 2, "Number of workers")
+	flag.Parse()
 
-  return Options{ url: *url, target: *target, workers: *workers }
+	return Options{url: *url, target: *target, workers: *workers}
 }
